6740623f850cce179d54e050fab39d02a1f187c3
Max Adaloudis <maxadaloudis@gmail.com>
2021-04-20 12:34:32 +0200

    Included preprocessing and data exploration

--- compress_files.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-pd.read_csv(
-    "./Data/Fact_bids1.csv",
-    sep=';').to_csv(
-        "./Data/Fact_bids1.csv.gz",
-    index=False)
-pd.read_csv("./Data/DIM_LOT.csv", sep=';').to_csv("./Data/DIM_LOT.csv.gz", index=False)
